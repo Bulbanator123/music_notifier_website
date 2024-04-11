05315012dbd444d2e9cb3d3ff50cae37398b7e9f
NotHolyMan <lebedevdanka4@gmail.com>
2024-04-11 23:34:05 +0300

    Добавлена разметка под роуты, новые функции. Написан html и css главной страницы. Приляпаны html-формы. Написана часть дб. Начата работа с api.

---
+++ get_api_token.py
@@ -0,0 +1,30 @@
+import dotenv
+import base64
+from requests import post
+import json
+import os
+
+dotenv.load_dotenv()
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+
+
+def get_api_token():
+    auth_string = CLIENT_ID + ":" + CLIENT_SECRET
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+
+token = get_api_token()
+print(token)
--- main.py
+++ main.py
@@ -1,20 +1,52 @@
 import os
 import requests
 import dotenv
-from flask import Flask
+from flask import Flask, render_template
+from data import db_session
+
 # ------импорты------
 
 dotenv.load_dotenv()
 
-TOKEN = os.environ.get("TOKEN")
-# ------константы-----
-app = Flask(__name__)
+SECRET_KEY = os.environ.get("SECRET_KEY")
+# ------ENV---------
+# ----константы-----
+
+app = Flask(__name__)  # создание приложения
+app.config['SECRET_KEY'] = SECRET_KEY
 
 
-@app.route('/') # главный роут
+@app.route('/')  # главный роут
 def main_route():
-    return "Музыкальный усведомитель"
+    params = {"title": "Music Notifier"}
+    return render_template("main.html", **params)
 
 
-if __name__ == '__main__': # что будет происходить приза пуске
+@app.route('/user')  # роут пользователя
+def user_route():
+    params = {"title": "Профиль"}
+    return render_template("login.html", **params)
+
+
+@app.route('/artist')  # роут артиста
+def artist_route():
+    params = {"title": "Артист"}
+    return render_template("artist.html", **params)
+
+
+@app.route('/artist/music')  # роут музыки
+def music_route():
+    params = {"title": "Музыка"}
+    return render_template("music.html", **params)
+
+
+async def update_API():  # обновление данных из api
+    pass
+
+
+def main():  # отдельно-вынесенная функция main
     app.run(port=8080, host='127.0.0.1')
+
+
+if __name__ == '__main__':  # что будет происходить при запуске
+    main()
